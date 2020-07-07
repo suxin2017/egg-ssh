1e0a020051c08f1fcc15fe68953509abad6be56e
AngelName <yishidixiashang@gmail.com>
2020-07-07 16:39:35 +0800

    feat: 添加类型

---
+++ index.d.ts
@@ -0,0 +1,24 @@
+import NodeSSH, {ConfigGiven, ExecOptions} from 'node-ssh';
+import SSH from "node-ssh";
+
+interface EggSSH extends NodeSSH {
+
+    constructor(initConfig: ConfigGiven)
+
+    autoConnect(config: ConfigGiven): EggSSH
+
+    autoConnectExecCommand(givenCommand: string, options: ExecOptions, config: ConfigGiven): Promise<SSH.ExecResult>
+}
+
+declare module 'egg' {
+
+    // extend app
+    interface Application {
+        ssh: EggSSH;
+    }
+
+    // extend your config
+    interface EggAppConfig {
+        ssh: NodeSSH.ConfigGiven;
+    }
+}
